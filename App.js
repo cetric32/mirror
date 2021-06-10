83bdf0c9fae405a87f6b2e91e89156e64ce13a24
Cetric Lihalakha <cetric32@gmail.com>
2021-06-10 13:06:27 +0300

    mirror project react native

---
+++ App.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Camera} from 'expo-camera';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [torchOn, setTorchOn] = useState(Camera.Constants.FlashMode.off);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} flashMode={torchOn}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setTorchOn(
+                torchOn === Camera.Constants.FlashMode.off
+                  ? Camera.Constants.FlashMode.torch
+                  : Camera.Constants.FlashMode.off,
+              );
+            }}>
+            <Text style={styles.text}> Torch </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  camera: {
+    height: '100%',
+  },
+});
